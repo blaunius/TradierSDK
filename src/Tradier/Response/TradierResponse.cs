232436154c521691b066d12c26520da61d48b1f8
blaunius <barrettlaunius@gmail.com>
2025-08-11 02:18:56 -0500

    add response models

---
+++ src/Tradier/Response/TradierResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tradier.Response
+{
+    public abstract class TradierResponse
+    {
+        public string RawResponse { get; internal set; } = null!;
+        public bool Successful { get; internal set; }
+        internal Task Parse(HttpResponseMessage rs)
+        {
+            RawResponse = rs.Content.ReadAsStringAsync().Result;
+            Successful = rs.IsSuccessStatusCode;
+            Deserialize();
+            return Task.CompletedTask;
+        }
+        internal virtual void Deserialize()
+        {
+            //this method should always be overriden
+            throw new NotImplementedException(this.GetType() + " is not yet implemented for serialization");
+        }
+    }
+}
---
+++ src/Tradier/Response/UserProfileResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tradier.Response
+{
+    public class UserProfileResponse : TradierResponse
+    {
+        public Model.Profile? Profile { get; set; }
+        internal override void Deserialize()
+        {
+            if (this.Successful)
+            {
+                this.Profile = Newtonsoft.Json.JsonConvert.DeserializeObject<UserProfileResponse>(this.RawResponse)?.Profile;
+            }
+        }
+    }
+}
