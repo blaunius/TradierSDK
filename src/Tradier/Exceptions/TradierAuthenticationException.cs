7404a19c4fbd13ebb3726d4372a8393ae956150f
blaunius <barrettlaunius@gmail.com>
2025-08-11 00:49:19 -0500

    add custom exception

---
+++ src/Tradier/Exceptions/TradierAuthenticationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tradier.Exceptions
+{
+    public class TradierAuthenticationException : System.Exception
+    {
+        public TradierAuthenticationException() : base("An Authentication Error has occurred")
+        {
+        }
+        public TradierAuthenticationException(string message) : base(message)
+        {
+        }
+    }
+}
