cbb0f2d31a8db3f3571b5eb4d3206c7ba01bd5a5
blaunius <barrettlaunius@gmail.com>
2025-08-10 22:09:39 -0500

    market data service base impl

--- src/Tradier/Services/MarketDataService.cs
+++ src/Tradier/Services/MarketDataService.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tradier.Model;
+﻿using Tradier.Model;
+using Tradier.Request;
 
 namespace Tradier.Services
 {
@@ -14,68 +10,122 @@ namespace Tradier.Services
         /// <summary>
         /// Get a list of symbols using a keyword lookup on the symbols description. Results are in descending order by average volume of the security. This can be used for simple search functions.
         /// </summary>
-        /// <returns>List of Quotes</returns>
-        public List<Quote> GetQuotes()
+        public Task<List<Quote>> GetQuotes(bool showGreeks, params string[] symbols)
         {
             throw new NotImplementedException();
         }
-
         /// <summary>
         /// Get all quotes in an option chain.
         /// </summary>
-        /// <returns></returns>
         /// <exception cref="NotImplementedException"></exception>
-        public List<Option> GetOptionChains()
+        public Task<List<Option>> GetOptionChains(string symbol, DateTime expiration, bool includeAllGreeks = false)
         {
             throw new NotImplementedException();
         }
-
-        public object GetOptionStrikes()
+        /// <summary>
+        /// Get an options strike prices for a specified expiration date.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<decimal>> GetOptionStrikes(string symbol, DateTime expiration, bool includeAllRoots = false)
         {
             throw new NotImplementedException();
         }
-
-        public object GetOptionExpirations()
+        /// <summary>
+        /// Get expiration dates for a particular underlying. 
+        /// Note that some underlying securities use a different symbol for their weekly options(RUT/RUTW, SPX/SPXW).
+        /// To make sure you see all expirations, make sure to send the includeAllRoots parameter.
+        /// This will also ensure any unique options due to corporate actions(AAPL1) are returned.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<Expiration>> GetOptionExpirations(string symbol, ExpirationLookupOptions? options = null)
+        {
+            options ??= new();
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get all options symbols for the given underlying. 
+        /// This will include additional option roots (ex. SPXW, RUTW) if applicable.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<OptionSymbol>> LookupOptionSymbols(string underlying)
         {
             throw new NotImplementedException();
         }
-
-        public object LookupOptionSymbols()
+        /// <summary>
+        /// Get historical pricing for a security.
+        /// This data will usually cover the entire lifetime of the company if sending reasonable start/end times.
+        /// You can fetch historical pricing for options by passing the OCC option symbol (ex. AAPL220617C00270000) as the symbol.
+        /// </summary>
+        /// <remarks>Notes: Historical data may not be dividend adjusted as this relies on the exchanges to report/adjust it properly.
+        /// Historical options data is not available for expired options.
+        /// </remarks>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<HistoricalQuote>> GetHistoricalQuotes(string symbol, HistoricalQuotesLookupOptions? options = null)
+        {
+            options ??= new();
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Time and Sales (timesales) is typically used for charting purposes.
+        /// It captures pricing across a time slice at predefined intervals.
+        /// </summary>
+        /// <remarks>
+        /// Tick data is also available through this endpoint.
+        /// This results in a very large data set for high-volume symbols, so the time slice needs to be much smaller to keep downloads time reasonable.
+        /// </remarks>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<TimeAndSales>> GetTimeAndSales(string symbol, TimeAndSalesOptions? options = null)
         {
             throw new NotImplementedException();
         }
-
-        public object GetHistoricalQuotes()
+        /// <summary>
+        /// The ETB list contains securities that are able to be sold short with a Tradier Brokerage account.
+        /// The list is quite comprehensive and can result in a long download response time.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<IEnumerable<ETBSecurity>> GetETBSecurities(CancellationToken token = default)
         {
             throw new NotImplementedException();
         }
-
-        public object GetTimeAndSales()
+        /// <summary>
+        /// Get the intraday market status. This call will change and return information pertaining to the current day.
+        /// If programming logic on whether the market is open/closed – this API call should be used to determine the current state.
+        /// </summary>        
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<Clock> GetClock(bool delayed = false)
         {
             throw new NotImplementedException();
         }
-
-        public object GetETBSecurities()
+        /// <summary>
+        /// Get the market calendar for the current or given month. This can be used to plan ahead regarding strategies.
+        /// However, the Get Intraday Status should be used to determine the current status of the market.
+        /// </summary>
+        /// <param name="month">Defaults to current month</param>
+        /// <param name="year">Defaults to current year</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<Calendar> GetCalendar(int? month = null, int? year = null)
+        {
+            month ??= DateTime.Now.Month;
+            year ??= DateTime.Now.Year;
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Get a list of symbols using a keyword lookup on the symbols description. 
+        /// Results are in descending order by average volume of the security. 
+        /// This can be used for simple search functions.
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<Security>> SearchCompanies(string searchQuery, bool showIndexes = true)
         {
             throw new NotImplementedException();
         }
-
-        public object GetClock()
-        {
-            throw new NotImplementedException();
-        }
-
-        public object GetCalendar()
-        {
-            throw new NotImplementedException();
-        }
-
-        public object SearchCompanies()
-        {
-            throw new NotImplementedException();
-        }
-
-        public object LookupSymbol()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="exchanges">A CSV string of exchanges</param>
+        /// <parm name="securityTypes">A CSV string of security types (Stock, option, etf, index)</param>
+        /// <exception cref="NotImplementedException"></exception>
+        public Task<List<Security>> LookupSymbol(string symbol, string securityTypes = "All", string exchanges = "All")
         {
             throw new NotImplementedException();
         }
