c9893c4593d328a3f2b21170945370ea7c40b228
blaunius <barrettlaunius@gmail.com>
2025-08-10 22:08:07 -0500

    add enums

---
+++ src/Tradier/Enumerations/IntervalType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tradier.Enumerations
+{
+    public enum IntervalType
+    {
+        Daily,
+        Weekly,
+        Monthly
+    }
+}
---
+++ src/Tradier/Enumerations/SessionFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tradier.Enumerations
+{
+    public enum SessionFilter
+    {
+        All,
+        Open
+    }
+}
