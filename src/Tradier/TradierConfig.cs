7157846a492618944d0b7ad733df5567ee41e46c
blaunius <barrettlaunius@gmail.com>
2025-08-10 23:48:08 -0500

    add TradierConfig for global client access

---
+++ src/Tradier/TradierConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#nullable disable
+namespace Tradier
+{
+    public static class TradierConfig
+    {
+        internal static bool IsSandboxActive { get; set; }
+        public static string AccessToken { get; set; }
+        public static string AccessTokenSandbox { get; set; }
+        public static string RedirectUri { get; set; }
+    }
+}
+#nullable restore
