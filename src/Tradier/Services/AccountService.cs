45a8535d06a06f858863a8cb8c24f2eacf7e458d
blaunius <barrettlaunius@gmail.com>
2025-08-18 22:00:47 -0500

    add cancellation tokens

--- src/Tradier/Services/AccountService.cs
+++ src/Tradier/Services/AccountService.cs
@@ -18,41 +18,41 @@ namespace Tradier.Services
                 throw new NotSupportedException("Account information can only be used in the production client.");
         }
 
-        public Task<AccountProfileResponse> GetUserProfile()
+        public Task<AccountProfileResponse> GetUserProfile(CancellationToken token = default)
         {
-            return client.GetResponse<AccountProfileResponse>("user/profile");
+            return client.GetResponse<AccountProfileResponse>("user/profile", token);
         }
 
-        public Task<AccountBalancesResponse> GetBalances(string accountId)
+        public Task<AccountBalancesResponse> GetBalances(string accountId, CancellationToken token = default)
         {
-            return client.GetResponse<AccountBalancesResponse>($"accounts/{accountId}/balances");
+            return client.GetResponse<AccountBalancesResponse>($"accounts/{accountId}/balances", token);
         }
 
-        public Task<AccountPositionsResponse> GetPositions(string accountId)
+        public Task<AccountPositionsResponse> GetPositions(string accountId, CancellationToken token = default)
         {
-            return client.GetResponse<AccountPositionsResponse>($"accounts/{accountId}/positions");
+            return client.GetResponse<AccountPositionsResponse>($"accounts/{accountId}/positions", token);
         }
 
-        public Task<AccountHistoryResponse> GetHistory(string accountId, GetHistoryRequestOptions? query = null)
+        public Task<AccountHistoryResponse> GetHistory(string accountId, GetHistoryRequestOptions? query = null, CancellationToken token = default)
         {
             query ??= new GetHistoryRequestOptions();
-            return client.GetResponse<AccountHistoryResponse>($"accounts/{accountId}/history?{query.ParseQueryString()}");
+            return client.GetResponse<AccountHistoryResponse>($"accounts/{accountId}/history?{query.ParseQueryString()}", token);
         }
 
-        public Task<AccountGainLossResponse> GetGainLoss(string accountId, GainLossOptions? query = null)
+        public Task<AccountGainLossResponse> GetGainLoss(string accountId, GainLossOptions? query = null, CancellationToken token = default)
         {
             query ??= new GainLossOptions();
-            return client.GetResponse<AccountGainLossResponse>($"accounts/{accountId}/gainloss?{query.ParseQueryString()}");
+            return client.GetResponse<AccountGainLossResponse>($"accounts/{accountId}/gainloss?{query.ParseQueryString()}", token);
         }
 
-        public Task<AccountOrdersResponse> GetOrders(string accountId, bool includeTags = false, int? page = null)
+        public Task<AccountOrdersResponse> GetOrders(string accountId, bool includeTags = false, int? page = null, CancellationToken token = default)
         {
-            return client.GetResponse<AccountOrdersResponse>($"accounts/{accountId}/orders?include_tags={includeTags.ToString().ToLowerInvariant()}{(page.HasValue ? $"&page={page.Value}" : "")}");
+            return client.GetResponse<AccountOrdersResponse>($"accounts/{accountId}/orders?include_tags={includeTags.ToString().ToLowerInvariant()}{(page.HasValue ? $"&page={page.Value}" : "")}", token);
         }
 
-        public Task<AccountOrderResponse> GetOrder(string accountId, string id, bool includeTags = false)
+        public Task<AccountOrderResponse> GetOrder(string accountId, string id, bool includeTags = false, CancellationToken token = default)
         {
-            return client.GetResponse<AccountOrderResponse>($"accounts/{accountId}/orders/{id}?include_tags={includeTags.ToString().ToLowerInvariant()}");
+            return client.GetResponse<AccountOrderResponse>($"accounts/{accountId}/orders/{id}?include_tags={includeTags.ToString().ToLowerInvariant()}", token);
         }
     }
 }
--- src/Tradier/Services/MarketDataService.cs
+++ src/Tradier/Services/MarketDataService.cs
@@ -19,16 +19,16 @@ namespace Tradier.Services
         /// <summary>
         /// Get all quotes in an option chain.
         /// </summary>
-        public Task<MarketOptionChainsResponse> GetOptionChains(string symbol, DateTime expiration, bool includeAllGreeks = false)
+        public Task<MarketOptionChainsResponse> GetOptionChains(string symbol, DateTime expiration, bool includeAllGreeks = false, CancellationToken token = default)
         {
-            return client.GetResponse<MarketOptionChainsResponse>($"markets/options/chains?symbol={symbol}&expiration={expiration:yyyy-MM-dd}&greeks={includeAllGreeks.ToString().ToLower()}");
+            return client.GetResponse<MarketOptionChainsResponse>($"markets/options/chains?symbol={symbol}&expiration={expiration:yyyy-MM-dd}&greeks={includeAllGreeks.ToString().ToLower()}", token);
         }
         /// <summary>
         /// Get an options strike prices for a specified expiration date.
         /// </summary>
-        public Task<MarketOptionStikesResponse> GetOptionStrikes(string symbol, DateTime expiration, bool includeAllRoots = false)
+        public Task<MarketOptionStikesResponse> GetOptionStrikes(string symbol, DateTime expiration, bool includeAllRoots = false, CancellationToken token = default)
         {
-            return client.GetResponse<MarketOptionStikesResponse>($"markets/options/strikes?symbol={symbol}&expiration={expiration:yyyy-MM-dd}&includeAllRoots={includeAllRoots.ToString().ToLower()}");
+            return client.GetResponse<MarketOptionStikesResponse>($"markets/options/strikes?symbol={symbol}&expiration={expiration:yyyy-MM-dd}&includeAllRoots={includeAllRoots.ToString().ToLower()}", token);
         }
         /// <summary>
         /// Get expiration dates for a particular underlying. 
@@ -36,18 +36,18 @@ namespace Tradier.Services
         /// To make sure you see all expirations, make sure to send the includeAllRoots parameter.
         /// This will also ensure any unique options due to corporate actions(AAPL1) are returned.
         /// </summary>
-        public Task<MarketOptionExpirationResponse> GetOptionExpirations(string symbol, MarketOptionExpirationRequest? options = null)
+        public Task<MarketOptionExpirationResponse> GetOptionExpirations(string symbol, MarketOptionExpirationRequest? options = null, CancellationToken token = default)
         {
             options ??= new();
-            return client.GetResponse<MarketOptionExpirationResponse>($"markets/options/expirations?symbol={symbol}&includeAllRoots={options.includeAllRoots}&contractSize={options.showContractSize}&expirationType={options.showExpirationType}&strikes={options.showStrikes}");
+            return client.GetResponse<MarketOptionExpirationResponse>($"markets/options/expirations?symbol={symbol}&includeAllRoots={options.includeAllRoots}&contractSize={options.showContractSize}&expirationType={options.showExpirationType}&strikes={options.showStrikes}", token);
         }
         /// <summary>
         /// Get all options symbols for the given underlying. 
         /// This will include additional option roots (ex. SPXW, RUTW) if applicable.
         /// </summary>
-        public Task<MarketOptionSymbolsResponse> LookupOptionSymbols(string underlying)
+        public Task<MarketOptionSymbolsResponse> LookupOptionSymbols(string underlying, CancellationToken token = default)
         {
-            return client.GetResponse<MarketOptionSymbolsResponse>($"markets/options/lookup?underlying={underlying}");
+            return client.GetResponse<MarketOptionSymbolsResponse>($"markets/options/lookup?underlying={underlying}", token);
         }
         /// <summary>
         /// Get historical pricing for a security.
@@ -57,10 +57,10 @@ namespace Tradier.Services
         /// <remarks>Notes: Historical data may not be dividend adjusted as this relies on the exchanges to report/adjust it properly.
         /// Historical options data is not available for expired options.
         /// </remarks>
-        public Task<MarketHistoricalQuotesResponse> GetHistoricalQuotes(string symbol, MarketHistoricalQuotesRequest? options = null)
+        public Task<MarketHistoricalQuotesResponse> GetHistoricalQuotes(string symbol, MarketHistoricalQuotesRequest? options = null, CancellationToken token = default)
         {
             options ??= new();
-            return client.GetResponse<MarketHistoricalQuotesResponse>($"markets/history?symbol={symbol}&{options.ParseQuery()}");
+            return client.GetResponse<MarketHistoricalQuotesResponse>($"markets/history?symbol={symbol}&{options.ParseQuery()}", token);
         }
         /// <summary>
         /// Time and Sales (timesales) is typically used for charting purposes.
@@ -70,10 +70,10 @@ namespace Tradier.Services
         /// Tick data is also available through this endpoint.
         /// This results in a very large data set for high-volume symbols, so the time slice needs to be much smaller to keep downloads time reasonable.
         /// </remarks>
-        public Task<MarketTimeAndSalesResponse> GetTimeAndSales(string symbol, MarketTimeAndSalesRequest? options = null)
+        public Task<MarketTimeAndSalesResponse> GetTimeAndSales(string symbol, MarketTimeAndSalesRequest? options = null, CancellationToken token = default)
         {
             options ??= new();
-            return client.GetResponse<MarketTimeAndSalesResponse>($"markets/timesales?symbol={symbol}&{options.ParseQuery()}");
+            return client.GetResponse<MarketTimeAndSalesResponse>($"markets/timesales?symbol={symbol}&{options.ParseQuery()}", token);
         }
         /// <summary>
         /// The ETB list contains securities that are able to be sold short with a Tradier Brokerage account.
@@ -87,9 +87,9 @@ namespace Tradier.Services
         /// Get the intraday market status. This call will change and return information pertaining to the current day.
         /// If programming logic on whether the market is open/closed – this API call should be used to determine the current state.
         /// </summary>        
-        public Task<MarketClockResponse> GetClock(bool delayed = false)
+        public Task<MarketClockResponse> GetClock(bool delayed = false, CancellationToken token = default)
         {
-            return client.GetResponse<MarketClockResponse>($"markets/clock?delayed={delayed.ToString().ToLower()}");
+            return client.GetResponse<MarketClockResponse>($"markets/clock?delayed={delayed.ToString().ToLower()}", token);
         }
         /// <summary>
         /// Get the market calendar for the current or given month. This can be used to plan ahead regarding strategies.
@@ -98,26 +98,26 @@ namespace Tradier.Services
         /// <param name="month">Defaults to current month</param>
         /// <param name="year">Defaults to current year</param>
         /// <exception cref="NotImplementedException"></exception>
-        public Task<MarketCalendarResponse> GetCalendar(int? month = null, int? year = null)
+        public Task<MarketCalendarResponse> GetCalendar(int? month = null, int? year = null, CancellationToken token = default)
         {
             month ??= DateTime.Now.Month;
             year ??= DateTime.Now.Year;
-            return client.GetResponse<MarketCalendarResponse>($"markets/calendar?month={month.Value.ToString("00")}&year={year}");
+            return client.GetResponse<MarketCalendarResponse>($"markets/calendar?month={month.Value.ToString("00")}&year={year}", token);
         }
         /// <summary>
         /// Get a list of symbols using a keyword lookup on the symbols description. 
         /// Results are in descending order by average volume of the security. 
         /// This can be used for simple search functions.
         /// </summary>
-        public Task<MarketSearchCompaniesResponse> SearchCompanies(string searchQuery, bool showIndexes = true)
+        public Task<MarketSearchCompaniesResponse> SearchCompanies(string searchQuery, bool showIndexes = true, CancellationToken token = default)
         {
-            return client.GetResponse<MarketSearchCompaniesResponse>($"markets/search?q={searchQuery}&indexes={showIndexes.ToString().ToLower()}");
+            return client.GetResponse<MarketSearchCompaniesResponse>($"markets/search?q={searchQuery}&indexes={showIndexes.ToString().ToLower()}", token);
         }
         /// <param name="exchanges">A CSV string of exchanges</param>
         /// <parm name="securityTypes">A CSV string of security types (Stock, option, etf, index, etc)</param>
-        public Task<MarketLookupSymbolResponse> LookupSymbol(string symbol, string securityTypes = "All", string exchanges = "All")
+        public Task<MarketLookupSymbolResponse> LookupSymbol(string symbol, string securityTypes = "All", string exchanges = "All", CancellationToken token = default)
         {
-            return client.GetResponse<MarketLookupSymbolResponse>($"markets/lookup?symbol={symbol}&types={securityTypes}&exchanges={exchanges}");
+            return client.GetResponse<MarketLookupSymbolResponse>($"markets/lookup?symbol={symbol}&types={securityTypes}&exchanges={exchanges}", token);
         }
     }
 }
